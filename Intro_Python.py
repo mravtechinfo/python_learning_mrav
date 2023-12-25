fa2a6780e9dab553ab6dd89bd7fc1458656d3b1c
Anubhav Miller <anubhavmiller777@gmail.com>
2023-12-26 02:54:08 +0530

    Initial commit for python learning journey

---
+++ Intro_Python.py
@@ -0,0 +1,17 @@
+#print function to print statements
+print('hello')
+
+#input function to take input from the user
+name = input('Enter your name here :') 
+print('Hello '+name)
+
+#Blank line can improve the readibility of your code so how to use that ?
+
+print('Hello world')
+
+#print statement without any statement will print a blank line for you
+print()
+print('did you see that blank line ?')
+#you can also use \n to have a blank line in the middle of line or string .
+print('Blank line \nin the middle of a string is like this.')
+
