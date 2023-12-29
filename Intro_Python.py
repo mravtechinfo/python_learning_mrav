d5d1818e26150deed42bc1aac0fc3302872e52d6
Anubhav Miller <anubhavmiller777@gmail.com>
2023-12-30 01:35:27 +0530

    third commit

--- Intro_Python.py
+++ Intro_Python.py
@@ -16,3 +16,20 @@ print('did you see that blank line ?')
 #blank line = new line (\n)
 print('Blank line \nin the middle of a string is like this.')
 
+#we can do mathematics in python or say simple calculation
+print('adding numbers')
+x=56+546
+print(x)
+
+print('dividing numbers')
+
+y=567/0
+
+print(y)
+#print satatement are very use full used in case of debugging or finding the error in your program.
+
+
+
+#comments are also very important to write comment and write what the particular peice of code is doing, 
+#dont have any direct impact on the code not to be executed we can say
+#like what a particular fuction do we can do comment in debugging also
