a80cdb89639dd1ea045f3c954dd8752017d50c66
Anubhav Miller <anubhavmiller777@gmail.com>
2023-12-30 01:50:10 +0530

    notes updated

---
+++ strings_intro.py
@@ -0,0 +1,12 @@
+first_name = 'Anubhav'
+last_name = 'Miller'
+
+
+#print the name with space between
+print (first_name + ' ' + last_name)
+
+#lower method 
+print('hello lowercase'+' ' + first_name.lower()+' '+ last_name.lower())
+
+# upper method
+print('hello uppercase'+' ' + first_name.upper()+' '+ last_name.upper())
