98a715ff8469224dd4a9cf0d787cd941bc66b6ca
Anubhav Miller <anubhavmiller777@gmail.com>
2023-12-31 00:39:06 +0530

    date topic started

--- dates_intro.py
+++ dates_intro.py
@@ -1,1 +1,12 @@
 #Dates
+# It is one of the most difficult data type to work in any programming language
+
+# We often need current date and time when logging errors and saving data
+
+# To get current date and time
+# we need to use the datetime library
+from datetime import datetime
+current_date = datetime. now()
+# the now function returns a datetime object
+print( 'Today is:' + str (current_date))
+
