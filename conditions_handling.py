405c51c6412b35d3ec4a6c8bcf1c37d915d110fb
Anubhav Miller <anubhavmiller777@gmail.com>
2024-01-05 00:14:21 +0530

    conditions_handling

---
+++ conditions_handling.py
@@ -0,0 +1,10 @@
+# Your code needs to have the ability to take different action based on different conditions
+
+price_of_product = input('Enter the price of product you bought ')
+
+if price_of_product>=10:
+    tax = 0.7
+    print(tax)
+else:
+    tax=0
+    print(tax)
