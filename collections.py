e381ad0197fad56d27b68fa532b76deabfdbda50
Anubhav Miller <anubhavmiller777@gmail.com>
2024-01-09 16:15:32 +0530

    study resume from collections

--- collections.py
+++ collections.py
@@ -1,3 +1,22 @@
 # List is a collection of items
 # List are always mutable menaing you can change them
+
+names=['Anubhav', 'Aditya', 'Amit','Vaibhav']
+scores=[]
+scores.append(200)
+scores.append(190)
+scores.append(300)
+scores.insert(0,10)
+print(scores)
+
+print(names)
+
+print(scores[1])
+print(names[0])
+
+
+
+
+
+
 # Array is also a collection of items
