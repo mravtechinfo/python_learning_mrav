65f69a8073055aa20ad754ecf50e91d89e3bd380
Anubhav Miller <anubhavmiller777@gmail.com>
2024-01-10 02:24:39 +0530

    intro added

---
+++ intro_of_python.py
@@ -0,0 +1,35 @@
+#print function to print statements
+print('hello')
+
+#input function to take input from the user
+name = input('Enter your name here :') 
+print('Hello '+name)
+
+#Blank line can improve the readibility of your code so how to use that ?
+
+print('Hello world')
+
+#print statement without any statement will print a blank line for you
+print()
+print('did you see that blank line ?')
+#you can also use \n to have a blank line in the middle of line or string .
+#blank line = new line (\n)
+print('Blank line \nin the middle of a string is like this.')
+
+#we can do mathematics in python or say simple calculation
+print('adding numbers')
+x=56+546
+print(x)
+
+print('dividing numbers')
+
+y=567/0
+
+print(y)
+#print satatement are very use full used in case of debugging or finding the error in your program.
+
+
+
+#comments are also very important to write comment and write what the particular peice of code is doing, 
+#dont have any direct impact on the code not to be executed we can say
+#like what a particular fuction do we can do comment in debugging also
