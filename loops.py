ddda6ee0f3ab2a381680851293d88b46e5a033fe
Anubhav Miller <anubhavmiller777@gmail.com>
2024-01-09 21:42:50 +0530

    loops started

---
+++ loops.py
@@ -0,0 +1,10 @@
+# Loop - doing a same task again and again
+# Loops through a collection 
+
+for name in ['Anubhav','Vaibhav','Amit', 'Abhishek','Achintya','Twinkle','Aditya']:
+    print(name)
+
+# Looping a number of times 
+
+for index  in range (0,10): # Here also 10 is not included in the range
+    print(index)
