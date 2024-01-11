7e3c940f4cc49dc7781b1ef64f02b6955e051123
Anubhav Miller <anubhavmiller777@gmail.com>
2024-01-11 15:03:05 +0530

    functions halfway done

--- functions.py
+++ functions.py
@@ -14,3 +14,16 @@ for x in range(0,10):
 print()
 print_time('loop completed')
 
+def get_initial_name(name):
+    initial=name[0:1]
+    return initial
+
+
+first_name=input('enter your First name: ')
+first_name_initial = get_initial_name(first_name)
+
+last_name=input('enter your Last name: ')
+last_name_initial=get_initial_name(last_name)
+
+
+print('Your name initials are: '+first_name_initial+' '+last_name_initial)
