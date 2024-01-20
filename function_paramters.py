e30a2575cb746be3a50e47216027bfdf27940d61
Anubhav Miller <anubhavmiller777@gmail.com>
2024-01-20 15:08:42 +0530

    function paramters

--- function_paramters.py
+++ function_paramters.py
@@ -1,4 +1,5 @@
-def get_initial(name, force_uppercase):
+def get_initial(name, force_uppercase):  
+    # here we can make the force_uppercase=True i.e by default we can give it a value
     if force_uppercase:
         initial= name[0:1].upper()
     else:
@@ -11,4 +12,4 @@ first_name=input('Enter your first name: ')
 first_name_initial=get_initial(first_name,True)
 # pass parameters in same order as you have written in the functions
 
-print(first_name_initial)
+print('Your name initial is: '+first_name_initial) 
