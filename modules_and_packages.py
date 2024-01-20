95c140c1f2c22ae1a85a8489ad523f2608b29fcb
Anubhav Miller <anubhavmiller777@gmail.com>
2024-01-20 16:55:45 +0530

    modules and packages started

---
+++ modules_and_packages.py
@@ -0,0 +1,16 @@
+# Modules
+# What is a module
+# A python file with functions, classes and other components
+
+# Why to use module
+# Break code down into resuseable structures
+
+# Creating a module
+
+# helpers.py
+
+def display_message(message, is_warning=false):
+    if is_warning:
+        print('Warning !')
+    else:
+        print(message)
