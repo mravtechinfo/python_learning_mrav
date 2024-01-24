bf2907be8151b66bfdddc093a2668490197964d2
Anubhav Miller <anubhavmiller777@gmail.com>
2024-01-24 15:37:44 +0530

    m&p continue

--- modules_and_packages.py
+++ modules_and_packages.py
@@ -1,5 +1,5 @@
 # Modules
-# What is a module
+# What is a module ?
 # A python file with functions, classes and other components
 
 # Why to use module
@@ -14,3 +14,17 @@ def display_message(message, is_warning=False):
         print('Warning !')
     else:
         print(message)
+
+# Packages
+# What are packages ?
+# Published collections of modules are called packages
+
+
+# Installing packages
+# Install an individual package
+# pip install colorama
+# install from a list of packages
+# pip install -r requirements.txt
+# requiremets.txt
+# colorama
+        
