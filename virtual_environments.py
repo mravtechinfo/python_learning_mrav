9ee64df5c3d7104ad4552b294d165ee3d472298b
Anubhav Miller <anubhavmiller777@gmail.com>
2024-01-24 15:55:05 +0530

    venv

---
+++ virtual_environments.py
@@ -0,0 +1,45 @@
+# Virtual environments
+
+# By default packages are installed globally
+# Here version management becomes a challenge
+
+# Virtual Environements can be use to contain and manage packages collections
+# Just like folder behind the scenes with all your packages
+
+# Creating a virtual environment
+
+# Install virtual environment
+# pip install vritualenv
+
+# windows systems
+# python -m venv <folder_name>
+
+# OSX/Linux (Bash)
+# virtualenv <folder_name>
+
+# Using virtual environment
+
+# Windows systems
+# cmd.exe
+# <folder_name>\Scripts\Activate.bat
+
+# Powershell
+# <folder_name>\Scripts\Activate.ps1
+
+# bash shell
+# . ./<folder_name>/Scripts/activate
+
+# OSX/Linux (bash)
+# <folder_name>/bin/activate
+
+# Installing package in a virtual environment 
+# Its exactly same as the default case
+
+# Install an individual package
+# pip install colorama
+
+# install from a list of packages
+# pip install -r requirements.txt
+
+# requiremets.txt
+# colorama
