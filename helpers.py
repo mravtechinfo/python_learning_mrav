2b3e285430da46b8db7e106cade47e70d0e5841f
Anubhav Miller <anubhavmiller777@gmail.com>
2024-01-24 17:43:30 +0530

    venv

--- helpers.py
+++ helpers.py
@@ -3,4 +3,6 @@ def display_message(message, is_warning=False):
     if is_warning:
         print(Fore.RED + message)
     else:
-        print(Fore.GREEN + message)
+        print(Fore.GREEN + message + Fore.RESET)
+
+# Fore.RESET to reset the color of terminal other wise it uses the last color for the whole terminal for the rest of the work you do 
