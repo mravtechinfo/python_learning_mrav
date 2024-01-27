6f5e30b10d4aaa7a366bf4987e80d450ac2bb19f
Anubhav Miller <anubhavmiller777@gmail.com>
2024-01-28 00:56:42 +0530

    api

---
+++ calling_an_api.py
@@ -0,0 +1,16 @@
+# What is a web service ??
+# When a developer wants to share the functionality of a function but not the actual code in the program, they can place the function on a web server. 
+# A programmer with the address of that function on the web server and the required permissions can call the function.
+# This is called a web service
+
+
+# What is an API
+# You can't call a function unless you know the function name and the required parameters
+# When you create a web service you create an Application Programming Interface (API)
+# The API defines the function names and parameters so others know how to call your function.
+
+# Keys allow me to track which users have permission to use my web service
+# A developer signs up on my web site, or buys a license for my software and is provided a unique key
+# When the developer calls my web service they provide their unique key and I am able to verify the key has been approved for calls to my web service.
+
+
