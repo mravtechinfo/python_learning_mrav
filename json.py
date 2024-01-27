356c56659cff74f18e279098bcd8b3f3594ffa3e
Anubhav Miller <anubhavmiller777@gmail.com>
2024-01-28 01:15:06 +0530

    JSON and key:value

---
+++ json.py
@@ -0,0 +1,56 @@
+# JSON 
+# Many web service return data as JSON (JavaScript Object Notation)
+# Its an standard format that can be itimidating at first glance
+# JSON contains key pairs ("Key":"Value")
+# {"key":"subkey0" "subvalueO", "subkey]". "subvaluel", ...}
+# {"key" [listvalueO, listvalue1, listvalue2, ...]}
+
+import json
+
+# Create a dictionary object
+person_dict = {'first': 'Christopher', 'last':'Harrison'}
+# Add additional key pairs to dictionary as needed
+person_dict['City']='Seattle'
+
+# Convert dictionary to JSON object
+person_json = json.dumps(person_dict)
+
+# Print JSON object
+print(person_json)
+
+import json
+
+# Create a dictionary object
+person_dict = {'first': 'Christopher', 'last':'Harrison'}
+# Add additional key pairs to dictionary as needed
+person_dict['City']='Seattle'
+
+# Create a list object of programming languages 
+languages_list = ['CSharp','Python','JavaScript']
+
+# Add list object to dictionary for the languages key
+person_dict['languages']= languages_list
+
+# Convert dictionary to JSON object
+person_json = json.dumps(person_dict)
+
+# Print JSON object
+print(person_json)
+
+import json
+
+# Create a dictionary object
+person_dict = {'first': 'Christopher', 'last':'Harrison'}
+# Add additional key pairs to dictionary as needed
+person_dict['City']='Seattle'
+
+# create a staff dictionary
+# assign a person to a staff position of program manager
+staff_dict ={}
+staff_dict['Program Manager']=person_dict
+# Convert dictionary to JSON object
+staff_json = json.dumps(staff_dict)
+
+# Print JSON object
+print(staff_json)
+
